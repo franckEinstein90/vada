dc567aaeb1eb1e04af6884e42eac0747e7d98241
Franck Binard <franckbinard@gmail.com>
2020-09-06 23:21:28 -0400

    fdsa

--- src/client/main.js
+++ src/client/main.js
@@ -1,16 +1,23 @@
 "use strict"; 
 
+const moment = require('moment'); // require
 
 
 
 $(document).ready(function(){
+
+  const lastVisit = localStorage.getItem('lastVisit', moment().format()); 
+  const timeStamp = moment().format(); 
+  localStorage.setItem('lastVisit', timeStamp); 
 
   const app = {
     meta : {
           name: 'vada', 
           side: 'client'
     }, 
-
+    user : {
+      lastVisit
+    }, 
     p5: {
       container : {
         name : 'p5Container', 
@@ -18,6 +25,7 @@ $(document).ready(function(){
       }
     }
   }
+
 
   //Loading functions written in c++
   require('./webAssembly/main').loadWebAssemblyFunctions( app ) 
--- src/client/ui/canvas.js
+++ src/client/ui/canvas.js
@@ -5,30 +5,50 @@
 "use strict"; 
 /********************************************************************/ 
 
+const blockNum = 30; 
+
 const Config_P5Js_Canvas = function( app ){
-
-  const blockNum = 30; 
+  $( "#approxStart" ).click(function() {
+    alert( "Handler for .click() called." );
+  });
   const [canvasWidth,canvasHeight]  = [app.cppFunctions.blockSize() * blockNum, 500]; 
-  const full = app.cppFunctions.full();  
-  const sketch = function(p) {
-        let x = 100;
-        let y = 100;
-      
-        p.setup = function() {
-          p.createCanvas(canvasWidth, canvasHeight);
-        };
+  const full = app.cppFunctions.full(); 
+  const FPS = 30; //frames per second 
+  const refreshInterval = 1000; 
 
-        p.draw = function() {
-          p.background(app.p5.container.backgroundColor);
-          p.fill(255);
-          for(let y=0;y<canvasHeight;y+=3){
-            p.line(0, y, canvasWidth, y);
-          }
-        };
-    };
+  let last = 0; 
+  const sketch = function( p ) {
 
-    app.p5.engine = new p5(sketch, window.document.getElementById(app.p5.container.name)); 
-    return app; 
+    const initGrid = function(){
+      return Array(canvasHeight).fill(0).map((_,cy) => [0, cy, p.random(canvasWidth), cy] )
+    }
+    const grid = initGrid(canvasWidth, canvasHeight);
+    const updateGrid = ()=>{
+      grid.forEach(a=>a[2]=p.random(canvasWidth))
+    }
+    p.setup = function() {
+      p.createCanvas(canvasWidth, canvasHeight);
+    }
+
+    p.draw = function() {
+
+      const moveDown = () => {if( cy < canvasHeight ){ cy+=3 } else { lineDirection = 'up' }}
+      const moveUp = () => {if( cy > 0 ){ cy-=3 } else { lineDirection = 'down' }}
+      p.background(app.p5.container.backgroundColor);
+      p.fill(255);
+       grid.forEach(l => {
+          p.line(...l);
+      })
+ 
+      if(p.millis() > last + refreshInterval ){
+        last = p.millis(); 
+       updateGrid(); 
+      }
+    }
+  }
+
+  app.p5.engine = new p5(sketch, window.document.getElementById(app.p5.container.name)); 
+  return app; 
 }
 
 module.exports = {
